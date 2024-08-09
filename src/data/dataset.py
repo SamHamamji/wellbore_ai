3176ead3577bf5b17a0fa008f14366273c4448f3
Sam Hamamji <samhamamji0@gmail.com>
2024-08-09 19:04:57 +0300

    Fix dataset.py issue of being ignored

---
+++ src/data/dataset.py
@@ -0,0 +1,26 @@
+import os
+
+import scipy
+import torch
+import torch.utils.data
+
+
+class WaveDataset(torch.utils.data.Dataset):
+    def __init__(
+        self,
+        data_dir: str,
+    ):
+        self.data_dir = data_dir
+
+        self.files = list(
+            map(lambda file: os.path.join(data_dir, file), os.listdir(data_dir))
+        )
+        self.files = list(filter(lambda s: s.endswith(".mat"), self.files))
+
+    def __len__(self):
+        return len(self.files)
+
+    def __getitem__(self, index: int):
+        file = self.files[index]
+        data = scipy.io.loadmat(file)
+        return data["wavearray_param"], data["vs_r"], data["vp_r"]
