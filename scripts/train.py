0c90704e4c4e0fe37b08f73cd15546b09814f411
Sam Hamamji <samhamamji0@gmail.com>
2024-08-12 17:54:37 +0300

    Setup progress bars

--- scripts/train.py
+++ scripts/train.py
@@ -6,6 +6,7 @@ import ignite.contrib
 import ignite.contrib.handlers
 import ignite.engine
 import ignite.handlers
+import ignite.handlers.tqdm_logger
 import ignite.metrics
 import ignite.metrics.regression.mean_absolute_relative_error
 import ignite.utils
@@ -26,6 +27,7 @@ parser.add_argument("--batch_size", type=int, default=1)
 parser.add_argument("--dataloader_workers", type=int, default=0)
 parser.add_argument("--input_path", type=str, default=None)
 parser.add_argument("--output_path", type=str, default=None)
+parser.add_argument("--splits", type=float, nargs="+", default=(0.7, 0.2, 0.1))
 parser.add_argument("--seed", type=int, default=0)
 
 
@@ -41,13 +43,13 @@ if __name__ == "__main__":
         dtype=torch.float32,
         transform=Stft(64),
     )
-    train_loader, test_loader, val_loader = (
+    train_loader, val_loader, test_loader = (
         torch.utils.data.DataLoader(
             ds_split,
             batch_size=args.batch_size,
             num_workers=args.dataloader_workers,
         )
-        for ds_split in split_dataset(ds, torch.ones(len(ds)), (0.7, 0.2, 0.1))
+        for ds_split in split_dataset(ds, torch.ones(len(ds)), args.splits)
     )
 
     x_shape, y_shape = map(lambda t: t.shape, ds[0])
@@ -68,43 +70,36 @@ if __name__ == "__main__":
     train_evaluator = ignite.engine.create_supervised_evaluator(model, val_metrics)
     val_evaluator = ignite.engine.create_supervised_evaluator(model, val_metrics)
 
-    def log_training_loss(engine: ignite.engine.Engine):
-        print(
-            f"Epoch[{engine.state.epoch}], Iter[{engine.state.iteration}] Loss: {engine.state.output:.2f}"
-        )
+    def epoch_completed(trainer: ignite.engine.Engine):
+        train_metrics = train_evaluator.run(train_loader).metrics
+        val_metrics = val_evaluator.run(val_loader).metrics
 
-    def log_training_results(trainer: ignite.engine.Engine):
-        metrics = train_evaluator.run(train_loader).metrics
-        print(f"Training Metrics - Epoch[{trainer.state.epoch}]: {metrics}")
-
-    def log_validation_results(trainer: ignite.engine.Engine):
-        metrics = val_evaluator.run(val_loader).metrics
-        print(f"Validation Metrics - Epoch[{trainer.state.epoch}]: {metrics}")
+        print(f"Training metrics: {train_metrics}")
+        print(f"Validation metrics: {val_metrics}")
+        print()
 
     model_checkpoint = ignite.handlers.ModelCheckpoint(
         "models",
         n_saved=2,
         filename_prefix="best",
-        score_function=lambda engine: -engine.state.metrics["mse"],
+        score_function=(lambda engine: -engine.state.metrics["mse"]),
         score_name="mse",
         global_step_transform=ignite.contrib.handlers.global_step_from_engine(trainer),
         require_empty=False,
     )
 
-    trainer.add_event_handler(
-        ignite.engine.Events.ITERATION_COMPLETED(every=100), log_training_loss
-    )
+    trainer.add_event_handler(ignite.engine.Events.EPOCH_COMPLETED, epoch_completed)
 
-    trainer.add_event_handler(
-        ignite.engine.Events.EPOCH_COMPLETED, log_training_results
-    )
+    train_pbar = ignite.handlers.tqdm_logger.ProgressBar()
+    train_pbar.attach(trainer, output_transform=lambda x: {"loss": x})
 
-    trainer.add_event_handler(
-        ignite.engine.Events.EPOCH_COMPLETED, log_validation_results
-    )
+    # val_pbar = ignite.handlers.tqdm_logger.ProgressBar()
+    # val_pbar.attach(val_evaluator, output_transform=lambda x: {"loss": x})
 
     val_evaluator.add_event_handler(
-        ignite.engine.Events.COMPLETED, model_checkpoint, {"model": model}
+        ignite.engine.Events.COMPLETED,
+        model_checkpoint,
+        {"model": model, "optimizer": optimizer},
     )
 
-    trainer.run(train_loader, max_epochs=3)
+    trainer.run(train_loader, max_epochs=args.epochs)
