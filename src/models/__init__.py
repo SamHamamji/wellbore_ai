018f4c7f8597b774d586b1c0d542b9033e465f5e
Sam Hamamji <samhamamji0@gmail.com>
2024-08-14 23:33:26 +0300

    Setup fft_cnn2d

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,15 +1,13 @@
 import torch
 
-from .cnn2d import WaveCnn2d
-from .cnn3d import WaveCnn3d
+from .wave_cnn2d import WaveCnn2d
 from .wave_mlp import WaveMlp
 from .fft_mlp import FftMlp
 
 models: dict[str, type[torch.nn.Module]] = {
-    "cnn2d": WaveCnn2d,
-    "cnn3d": WaveCnn3d,
+    "wave_cnn2d": WaveCnn2d,
     "wave_mlp": WaveMlp,
     "fft_mlp": FftMlp,
 }
 
-__all__ = ["WaveCnn2d", "WaveCnn3d", "WaveMlp", "models"]
+__all__ = ["WaveCnn2d", "FftMlp", "WaveMlp", "models"]
---
+++ src/models/fft_cnn2d.py
@@ -0,0 +1,42 @@
+import torch
+
+from src.layers.fft import FftLayer
+
+
+class FftCnn2d(torch.nn.Sequential):
+    dataset_transform = torch.nn.Sequential(
+        torch.nn.LazyBatchNorm1d(),
+        FftLayer(-1, -3),
+    )
+
+    def __init__(self, input_shape: torch.Size, output_shape: torch.Size):
+        kernel_size = (3, 9)
+        conv_padding = (1, 0)
+
+        conv_layers = [
+            torch.nn.Conv2d(2, 4, kernel_size, 1, conv_padding),
+            # torch.nn.ReLU(),
+            torch.nn.MaxPool2d((3, 3), (3, 3)),
+            # torch.nn.Conv2d(4, 8, kernel_size, 1, conv_padding),
+            # torch.nn.ReLU(),
+            # torch.nn.MaxPool2d((3, 3), (3, 3)),
+        ]
+
+        linear_layers = [
+            # torch.nn.Linear(20046, 64),
+            torch.nn.Linear(4064, 64),
+            torch.nn.ReLU(),
+            # torch.nn.Linear(64, 32),
+            # torch.nn.ReLU(),
+            # torch.nn.Linear(32, 16),
+            # torch.nn.ReLU(),
+            torch.nn.Linear(64, 2),
+            torch.nn.ReLU(),
+            torch.nn.Linear(2, output_shape.numel()),
+        ]
+
+        super().__init__(
+            *conv_layers,
+            torch.nn.Flatten(-3, -1),
+            *linear_layers,
+        )
