f6e153b69177a9c4fc4fcc03b495784b4b8ff6ac
Sam Hamamji <samhamamji0@gmail.com>
2024-08-15 12:26:06 +0300

    Apply batch normalization to wave_mlp

--- src/models/wave_mlp.py
+++ src/models/wave_mlp.py
@@ -2,6 +2,8 @@ import torch
 
 
 class WaveMlp(torch.nn.Sequential):
+    dataset_transform = torch.nn.LazyBatchNorm1d()
+
     def __init__(self, input_shape: torch.Size, output_shape: torch.Size):
         layers = [
             torch.nn.Flatten(-len(input_shape), -1),
