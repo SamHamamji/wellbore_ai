94b95b472e6749ded5665efb8ea867183ade59fd
Sam Hamamji <samhamamji0@gmail.com>
2024-08-15 17:55:25 +0300

    Fix import issue in `models/__init__.py`

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,13 +1,13 @@
 import torch
 
-from .wave_cnn2d import WaveCnn2d
+from .fft_cnn2d import FftCnn2d
 from .wave_mlp import WaveMlp
 from .fft_mlp import FftMlp
 
 models: dict[str, type[torch.nn.Module]] = {
-    "wave_cnn2d": WaveCnn2d,
+    "fft_cnn2d": FftCnn2d,
     "wave_mlp": WaveMlp,
     "fft_mlp": FftMlp,
 }
 
-__all__ = ["WaveCnn2d", "FftMlp", "WaveMlp", "models"]
+__all__ = ["FftCnn2d", "FftMlp", "WaveMlp", "models"]
