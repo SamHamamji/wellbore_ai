695f9c14fe3c9bdbc2f50c45cb7bf996774d6771
Sam Hamamji <samhamamji0@gmail.com>
2024-08-15 14:54:50 +0300

    Clean up fft_cnn2d

--- src/models/fft_cnn2d.py
+++ src/models/fft_cnn2d.py
@@ -15,21 +15,12 @@ class FftCnn2d(torch.nn.Sequential):
 
         conv_layers = [
             torch.nn.Conv2d(2, 4, kernel_size, 1, conv_padding),
-            # torch.nn.ReLU(),
             torch.nn.MaxPool2d((3, 3), (3, 3)),
-            # torch.nn.Conv2d(4, 8, kernel_size, 1, conv_padding),
-            # torch.nn.ReLU(),
-            # torch.nn.MaxPool2d((3, 3), (3, 3)),
         ]
 
         linear_layers = [
-            # torch.nn.Linear(20046, 64),
             torch.nn.Linear(4064, 64),
             torch.nn.ReLU(),
-            # torch.nn.Linear(64, 32),
-            # torch.nn.ReLU(),
-            # torch.nn.Linear(32, 16),
-            # torch.nn.ReLU(),
             torch.nn.Linear(64, 2),
             torch.nn.ReLU(),
             torch.nn.Linear(2, output_shape.numel()),
