2311da8b909de93e301a0d17115ec574f33fac6c
Sam Hamamji <samhamamji0@gmail.com>
2024-08-17 16:54:57 +0300

    Setup amplitude fft cnn2d

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,11 +1,13 @@
 import torch
 
+from .amplitude_fft_cnn2d import AmplitudeFftCnn2d
 from .fft_cnn2d import FftCnn2d
 from .fft_mlp import FftMlp
 from .polar_fft_cnn2d import PolarFftCnn2d
 from .wave_mlp import WaveMlp
 
 models: dict[str, type[torch.nn.Module]] = {
+    "amplitude_fft_cnn2d": AmplitudeFftCnn2d,
     "fft_cnn2d": FftCnn2d,
     "fft_mlp": FftMlp,
     "polar_fft_cnn2d": PolarFftCnn2d,
--- src/models/amplitude_fft_cnn2d.py
+++ src/models/amplitude_fft_cnn2d.py
@@ -1,34 +1,30 @@
 import torch
 
-from src.layers.fft import FftLayer
+from src.layers import FftLayer, SelectIndexLayer
 
 
 class AmplitudeFftCnn2d(torch.nn.Sequential):
     dataset_x_transform = torch.nn.Sequential(
         torch.nn.LazyBatchNorm1d(),
         FftLayer(-1, -3, polar_decomposition=True),
+        SelectIndexLayer(-3, (slice(0, 1), slice(None), slice(None))),
     )
 
-    # pylint: disable=W0613
     def __init__(self, input_shape: torch.Size, output_shape: torch.Size):
         kernel_size = (3, 9)
         conv_padding = (1, 0)
 
         conv_layers = [
+            torch.nn.Conv2d(1, 2, kernel_size, 1, conv_padding),
+            torch.nn.MaxPool2d((2, 2), (2, 2)),
             torch.nn.Conv2d(2, 4, kernel_size, 1, conv_padding),
-            torch.nn.BatchNorm2d(4),
-            torch.nn.MaxPool2d((3, 3), (3, 3)),
-            torch.nn.Conv2d(4, 8, kernel_size, 1, conv_padding),
-            torch.nn.BatchNorm2d(8),
-            torch.nn.AvgPool2d((2, 2), (2, 2)),
+            torch.nn.MaxPool2d((2, 2), (2, 2)),
         ]
 
         linear_layers = [
-            torch.nn.Linear(1968, 64),
+            torch.nn.Linear(2232, 64),
             torch.nn.ReLU(),
-            torch.nn.Linear(64, 2),
-            torch.nn.ReLU(),
-            torch.nn.Linear(2, output_shape.numel()),
+            torch.nn.Linear(64, output_shape.numel()),
         ]
 
         super().__init__(
