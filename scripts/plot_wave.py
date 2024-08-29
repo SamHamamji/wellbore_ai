e8afedca4b36b930e939dd280d22e3c1879431e2
Sam Hamamji <samhamamji0@gmail.com>
2024-08-29 10:36:40 +0300

    Show frequency values in Hz in plot_wave script

--- scripts/plot_wave.py
+++ scripts/plot_wave.py
@@ -20,21 +20,37 @@ def plot_fft(wave: torch.Tensor, transform: torch.nn.Module):
     with torch.no_grad():
         spect = transform(wave)
 
+    sample_rate = 6.63130e-06
+    # pylint: disable=not-callable
+    frequencies = torch.fft.rfftfreq(wave.shape[-1], d=sample_rate)
+    time = torch.arange(wave.shape[-1]) * sample_rate
+
     app = dash.Dash()
-    app.layout = [
-        dash.html.Div(
+
+    app.layout = []
+    for receiver in [0, 4, 8, 12]:
+        signal = px.line(
+            x=time, y=wave[receiver], labels={"x": "Time", "y": "Amplitude"}
+        )
+        channel_1 = px.line(
+            x=frequencies, y=spect[receiver, :, 0], labels={"x": "Frequency (Hz)"}
+        )
+        channel_2 = px.line(
+            x=frequencies, y=spect[receiver, :, 1], labels={"x": "Frequency (Hz)"}
+        )
+        div = dash.html.Div(
             [
                 dash.html.H1(f"Receiver {receiver}"),
                 dash.html.H3("Raw signal"),
-                dash.dcc.Graph(figure=px.line(y=wave[receiver])),
-                dash.html.H3("Amplitude"),
-                dash.dcc.Graph(figure=px.line(y=spect[receiver, :, 0])),
-                dash.html.H3("Phase"),
-                dash.dcc.Graph(figure=px.line(y=spect[receiver, :, 1])),
+                dash.dcc.Graph(figure=signal),
+                dash.html.H3("Channel 1 (Cosine / Amplitude)"),
+                dash.dcc.Graph(figure=channel_1),
+                dash.html.H3("Channel 2 (Sine / Phase)"),
+                dash.dcc.Graph(figure=channel_2),
             ]
         )
-        for receiver in [0, 4, 8, 12]
-    ]
+        app.layout.append(div)
+
     app.run(debug=True)
 
 
@@ -51,7 +67,7 @@ if __name__ == "__main__":
     transform = torch.nn.Sequential(
         torch.nn.LazyBatchNorm1d(),
         FftLayer(-1, -1, args.polar),
-        SelectIndexLayer((slice(None), slice(200), slice(None))),
+        SelectIndexLayer((slice(None), slice(None), slice(None))),
     )
     x, y = ds[args.sample_index]
 
