962b8fc51b9bf10654e4669bb1693ccdb76b0e10
Sam Hamamji <samhamamji0@gmail.com>
2024-09-08 16:15:07 +0300

    Allow testing multiple models in test script

--- scripts/test.py
+++ scripts/test.py
@@ -11,7 +11,7 @@ from src.checkpoint import load_checkpoint
 
 parser = argparse.ArgumentParser()
 
-parser.add_argument("--model_path", type=str, required=True)
+parser.add_argument("--paths", type=str, required=True, nargs="+")
 parser.add_argument("--dataloader_workers", type=int, default=0)
 parser.add_argument("--batch_size", type=int, default=1)
 parser.add_argument("--test", action="store_true")
@@ -25,13 +25,6 @@ if __name__ == "__main__":
     np.random.seed(args.seed)
     torch.manual_seed(args.seed)
 
-    ds, model, _, scheduler = load_checkpoint(args.model_path)
-
-    print(f"Epoch: {scheduler.last_epoch}")
-    print(f"Dataset x transform: {ds.x_transform}")
-    print(f"Dataset y bounds: {ds.bounds}")
-    print()
-
     metrics: dict[str, typing.Callable[[torch.Tensor, torch.Tensor], torch.Tensor]] = {
         "rmse": lambda y, pred: (pred - y).square().mean(0).sqrt(),
         "mae": lambda y, pred: (pred - y).abs().mean(0),
@@ -40,20 +33,28 @@ if __name__ == "__main__":
         "relative error std": lambda y, pred: ((pred - y) / y).std(0),
     }
 
-    for split_name, ds_split in zip(
-        ("Train", "Validation", "Test"),
-        split_dataset(ds, torch.ones(len(ds)), args.splits),
-    ):
-        if split_name == "Test" and not args.test:
-            continue
+    for path in args.paths:
+        ds, model, _, scheduler = load_checkpoint(path)
 
-        loader = torch.utils.data.DataLoader(
-            ds_split,
-            batch_size=args.batch_size,
-            num_workers=args.dataloader_workers,
-        )
-
-        print(f"{split_name} metrics:")
-        for metric_name, result in test(loader, model, metrics).items():
-            print(f"\t{metric_name}: {result}")
+        print(f"Epoch: {scheduler.last_epoch}")
+        print(f"Dataset x transform: {ds.x_transform}")
+        print(f"Dataset y bounds: {ds.bounds}")
         print()
+
+        for split_name, ds_split in zip(
+            ("Train", "Validation", "Test"),
+            split_dataset(ds, torch.ones(len(ds)), args.splits),
+        ):
+            if split_name == "Test" and not args.test:
+                continue
+
+            loader = torch.utils.data.DataLoader(
+                ds_split,
+                batch_size=args.batch_size,
+                num_workers=args.dataloader_workers,
+            )
+
+            print(f"{split_name} metrics ({len(ds_split)} samples):")
+            for metric_name, result in test(loader, model, metrics).items():
+                print(f"\t{metric_name}: {result}")
+            print()
