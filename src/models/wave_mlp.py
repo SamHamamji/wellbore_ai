189974884a0e638ef3a28857c677305bcf782b69
Sam Hamamji <samhamamji0@gmail.com>
2024-09-25 18:25:22 +0300

    Update wave_mlp

--- src/models/wave_mlp.py
+++ src/models/wave_mlp.py
@@ -2,20 +2,35 @@ import torch
 
 
 class WaveMlp(torch.nn.Sequential):
-    dataset_x_transform = torch.nn.LazyBatchNorm1d()
+    dataset_x_transform = torch.nn.Sequential(
+        torch.nn.LazyBatchNorm1d(),
+    )
 
     def __init__(self, input_shape: torch.Size, output_shape: torch.Size):
         layers = [
             torch.nn.Flatten(-len(input_shape), -1),
-            torch.nn.Linear(input_shape.numel(), 64, dtype=torch.float32),
+            #
+            torch.nn.Linear(input_shape.numel(), 512),
             torch.nn.ReLU(),
-            torch.nn.Linear(64, 32, dtype=torch.float32),
+            torch.nn.LayerNorm(512),
+            torch.nn.Dropout(0.2),
+            #
+            torch.nn.Linear(512, 512),
             torch.nn.ReLU(),
-            torch.nn.Linear(32, 16, dtype=torch.float32),
+            torch.nn.LayerNorm(512),
+            torch.nn.Dropout(0.2),
+            #
+            torch.nn.Linear(512, 512),
             torch.nn.ReLU(),
-            torch.nn.Linear(16, 2, dtype=torch.float32),
+            torch.nn.LayerNorm(512),
+            torch.nn.Dropout(0.2),
+            #
+            torch.nn.Linear(512, 512),
             torch.nn.ReLU(),
-            torch.nn.Linear(2, output_shape.numel(), dtype=torch.float32),
+            torch.nn.LayerNorm(512),
+            torch.nn.Dropout(0.2),
+            #
+            torch.nn.Linear(512, output_shape.numel()),
         ]
 
         super().__init__(*layers)
