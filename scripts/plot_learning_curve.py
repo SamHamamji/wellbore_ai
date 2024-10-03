3317f280f34bfab1d5dac5a96ca138f696503811
Sam Hamamji <samhamamji0@gmail.com>
2024-10-03 18:12:29 +0300

    Separate plot_learning_curve from show script

--- scripts/plot_learning_curve.py
+++ scripts/plot_learning_curve.py
@@ -1,12 +1,19 @@
 import argparse
 
 from src.checkpoint import Checkpoint
+from src.plotter_engines import plotter_engines
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--path", type=str, required=True)
+parser.add_argument(
+    "--engine",
+    type=str,
+    default=plotter_engines.__args__[0],
+    choices=plotter_engines.__args__,
+)
 
 if __name__ == "__main__":
     args = parser.parse_args()
 
     checkpoint = Checkpoint.load_from_path(args.path)
-    checkpoint.print()
+    checkpoint.history.plot(args.engine)
